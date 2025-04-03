e269ac3f4e96942839139881a1d8cf58bb1f19d7
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2025-04-03 19:53:08 +0530

    updtae

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 const app = express();
+import cors from 'cors';
 
 const upload = multer({ dest: 'public/models/' });
 
 app.use(express.static('public'));
+app.use(cors({
+  origin: '*'
+}));
 
 app.post('/api/upload', upload.single('model'), (req, res) => {
   const fileUrl = `${req.protocol}://${req.get('host')}/models/${req.file.filename}`;
